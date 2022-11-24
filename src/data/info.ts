dc213f710ff1a0c2df9c7f82f92794bde9a3ce78
Mustafa Savul <mustafa.savul@crowdsol.com>
2022-11-24 15:01:20 +0300

    feat: init repo

---
+++ src/data/info.ts
@@ -0,0 +1,97 @@
+export const info = {
+  baseUrl: "",
+  name: "Mustafa Savul",
+  jobDescription: "Front End Developer",
+  about: `I'm a Front End developer based in Istanbul.
+  I'm passionate about building web applications with modern frameworks.
+  `,
+
+  experience: [
+    {
+      name: "Crowdsol",
+      location: "London/Istanbul Remote",
+      startDate: "Dec 2021",
+      endDate: "Present",
+      description: ["✓ Next.js (React.js)", "✓ Typescript", "✓ State Management", "✓ Styled Component with Storybook", "✓ Establishment of the front-end infrastructures of the new project", "✓ Amazon Interactive Video Service", "✓ Micro Front End (Lerna etc.), Node.js", "✓ Unit Test (Jest)", "✓ Introvoke", "✓ CI/CD Process"]
+    },
+
+    {
+      name: "Glomil Teknoloji",
+      location: "Istanbul, Turkey Hybrid",
+      startDate: "Jul 2021",
+      endDate: "Nov 2021",
+      description: ["✓ React,", "✓ Next.js,", "✓ Typescript", "✓ React hooks", "✓ Atlassian UI Kit", "✓ Projects were analyzed and components were designed and coded accordingly.", "✓ Cross-Browser"]
+    },
+
+    {
+      name: "OGOO Digital",
+      location: "Istanbul, Turkey Hybrid",
+      startDate: "Jun 2018",
+      endDate: "Jun 2021",
+      description: ["✓ Understandable and developable codes were written for applications.", "✓ Worked with project managers, project coordinator and other software teams.", "✓ Bug fixes for legacy and new standard applications.", "✓ Appropriate effort, reporting and quick solutions in project planning provided.", "✓ Opportunity to work on Microsoft technologies .NET MVC, Sharepoint etc.", "✓ Projects were analyzed and components were designed and coded accordingly.", "✓ Over time, new developments and technologies are available to developers and", "offered to managers.", "✓ Collaborate with customers and other developers to resolve technical issues", "worked together.", "✓ Site optimizations were made to improve search engines and browser performance. LCP etc.", "✓ Tests were conducted in virtual test environments such as BrowserStack.", "✓ Designs in Zeplin, Invision, Figma, Avocode, XD environments were dressed.", "✓ Cross-Browser support introduced IE9+"]
+    },
+  ],
+
+  education: [
+    {
+      name: "Anadolu University -  Management Information Systems (Open Education)",
+      location: "Eskişehir, Turkey",
+      startDate: "2021",
+      endDate: "2023",
+      description: [
+        "Open Education (There are no barriers to my work.)",
+      ],
+    },
+    {
+      name: "Istanbul Ayvansaray University -  Computer Programming",
+      location: "Istanbul, Turkey",
+      startDate: "2017",
+      endDate: "2020",
+      description: ["Computer Programming"],
+    },
+    {
+      name: "Ahi Evran University -  Computer Programming",
+      location: "Kırşehir, Turkey",
+      startDate: "2016",
+      endDate: "2017",
+      description: ["Horizontal transfer was made to Ayvansaray University."],
+    },
+  ],
+
+  socialMedia: {
+    twitter: "https://twitter.com/mustafasavull",
+    github: "https://github.com/mustafasavul",
+    linkedin: "https://www.linkedin.com/in/mustafasavul",
+  },
+
+  projects: [
+    {
+      title: "Crowdsol Event Service",
+      isFeatured: true,
+      thumbnail: "/assets/images/crowdsol.png",
+      githubUrl: "",
+      liveUrl: "https://app.crowdsol.com/login",
+    },
+    {
+      title: "Atatürk Cultural Center",
+      isFeatured: true,
+      thumbnail: "/assets/images/akm-istanbul.png",
+      githubUrl: "",
+      liveUrl: "https://akmistanbul.gov.tr/",
+    },
+    {
+      title: "GoKonya",
+      isFeatured: true,
+      thumbnail: "/assets/images/gokonya.png",
+      githubUrl: "",
+      liveUrl: "https://gokonya.com/tr/anasayfa",
+    },
+    {
+      title: "Other Projects Linkedin",
+      isFeatured: true,
+      thumbnail: "/assets/images/linkedin.png",
+      githubUrl: "",
+      liveUrl: "https://www.linkedin.com/in/mustafasavul/",
+    },
+  ],
+};
